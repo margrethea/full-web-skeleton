8c867d10d8647d74b7bd7767a79a84d84d1ec844
Jenny Hurum <jennyh@student.uia.no>
2020-10-16 16:24:20 +0200

    Ordnet øvelsesmodell til junior A og senior. La også til små funksjoner på servletene til junior A og B

--- src/main/java/servlets/JuniorA.java
+++ src/main/java/servlets/JuniorA.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "JuniorA")
+@WebServlet(name = "JuniorA", urlPatterns = "/servlet.JuniorA")
 public class JuniorA extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
