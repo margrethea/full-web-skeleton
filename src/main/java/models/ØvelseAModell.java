98b4b8e9b898dff37871c504975d428b14e733a1
Maren Ryder Lislevand <marenrl@uia.no>
2020-10-19 10:15:59 +0200

    endringer i øvelsesmodell junior A

---
+++ src/main/java/models/ØvelseAModell.java
@@ -0,0 +1,119 @@
+package models;
+
+public class ØvelseAModell {
+    private String forNavn;
+    private String etterNavn;
+    private String seksti;
+    private String beveglighet;
+    private String femtusenW;
+    private String femtusenT;
+    private String totusenW;
+    private String totusenT;
+    private String liggendeRoingKG;
+    private String liggendeRoingP;
+    private String sargeant;
+
+    public ØvelseAModell(String seksti, String beveglighet, String femtusenW, String femtusenT, String totusenW, String totusenT, String liggendeRoingP, String liggendeRoingKg, String sargeant) {
+        this.forNavn = forNavn;
+        this.etterNavn = etterNavn;
+        this.seksti = seksti;
+        this.beveglighet = beveglighet;
+        this.femtusenT = femtusenW;
+        this.femtusenW = femtusenT;
+        this.totusenW = totusenW;
+        this.totusenT = totusenT;
+        this.liggendeRoingKG = liggendeRoingKg;
+        this.liggendeRoingP = liggendeRoingP;
+        this.sargeant = sargeant;
+
+    }
+
+    /** Getters og setters*/
+
+    public String getForNavn(){
+        return forNavn;
+    }
+
+    public void setForNavn(String forNavn) {
+        this.forNavn = forNavn;
+    }
+
+     public String getEtterNavn(){
+        return etterNavn;
+     }
+
+     public void setEtterNavn(String etterNavn) {
+        this.etterNavn = etterNavn;
+     }
+
+    public String getSeksti() {
+        return seksti;
+    }
+
+    public void setSeksti(String seksti) {
+        this.seksti = seksti;
+    }
+
+    public String getBeveglighet() {
+        return beveglighet;
+    }
+
+    public void setBeveglighet(String beveglighet) {
+        this.beveglighet = beveglighet;
+    }
+
+    public String getFemtusenW() {
+        return femtusenW;
+    }
+
+    public void setFemtusenW(String femtusenW) {
+        this.femtusenW = femtusenW;
+    }
+
+    public String getFemtusenT() {
+        return femtusenT;
+    }
+
+    public void setFemtusenT(String femtusenT) {
+        this.femtusenT = femtusenT;
+    }
+
+    public String getTotusenW() {
+        return totusenW;
+    }
+
+    public void setTotusenW(String totusenW) {
+        this.totusenW = totusenW;
+    }
+
+    public String getTotusenT() {
+        return totusenT;
+    }
+
+    public void setTotusenT(String totusenT) {
+        this.totusenT = totusenT;
+    }
+
+    public String getLiggendeRoingKG() {
+        return liggendeRoingKG;
+    }
+
+    public void setLiggendeRoingKG(String liggendeRoingKG) {
+        this.liggendeRoingKG = liggendeRoingKG;
+    }
+    public String getLiggendeRoingP() {
+        return liggendeRoingP;
+    }
+
+    public void setLiggendeRoingP(String liggendeRoingP) {
+        this.liggendeRoingP = liggendeRoingP;
+    }
+
+    public String getSargeant() {
+        return sargeant;
+    }
+
+    public void setSargeant(String sargeant) {
+        this.sargeant = sargeant;
+    }
+}
