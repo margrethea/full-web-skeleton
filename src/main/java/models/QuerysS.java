0a3f6caf56d2a2571951fac734229999a1ea865d
Jørgen <skreosens@gmail.com>
2020-11-04 12:18:48 +0100

    La til riktig klasse på de forskjellige søkene

--- src/main/java/models/QuerysS.java
+++ src/main/java/models/QuerysS.java
@@ -6,7 +6,7 @@ public class QuerysS {
     }
 
     public static String alleResultat(String query){
-        query = ("select bruker.fornavn, s_resultat.år, s_resultat.periode, s_resultat.60w, s_resultat.bevegelighet, s_resultat.5000w, s_resultat.5000t, s_resultat.2000w, s_resultat.2000t, s_resultat.ligg_ro_kg, s_resultat.ligg_ro_p, s_resultat.knebøy_kg, s_resultat.knebøy_p, s_resultat.toppscore FROM ro.bruker, ro.s_resultat WHERE s_resultat.bruker_id=bruker.bruker_id ORDER BY s_resultat.år;");
+        query = ("select bruker.fornavn, s_resultat.år, s_resultat.periode, s_resultat.60w, s_resultat.bevegelighet, s_resultat.5000w, s_resultat.5000t, s_resultat.2000w, s_resultat.2000t, s_resultat.ligg_ro_kg, s_resultat.ligg_ro_p, s_resultat.knebøy_kg, s_resultat.knebøy_p, s_resultat.toppscore FROM ro.bruker, ro.s_resultat WHERE s_resultat.bruker_id=bruker.bruker_id AND bruker.klasse_id = 1 ORDER BY s_resultat.år;");
 
         return query;
     }
