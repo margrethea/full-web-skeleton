73787d9ebfe026570302746727eb9dc9d937c3b3
Rikke Solvang <rikke_solvang@hotmail.com>
2020-11-11 15:51:27 +0100

    søke på forskjellige parametre for junior C

--- src/main/java/models/Query/QuerysC.java
+++ src/main/java/models/Query/QuerysC.java
@@ -11,15 +11,70 @@ public class QuerysC {
 
     }
 
-    public static String alleResultat(String query){
-      query = ("select bruker.fornavn, c_resultat.år, c_resultat.periode, c_resultat.60w, c_resultat.bevegelighet, c_resultat.3000m, c_resultat.kroppshev, c_resultat.sargeant FROM ro.bruker, ro.c_resultat WHERE c_resultat.bruker_id=bruker.bruker_id AND bruker.klasse_id = 4 ORDER BY c_resultat.år;");
+    // Query som henter ut alle resultat for en utøver, og matcher det med bruker_id
+    //Sorteres etter år
+    //Brukes i klassen SearchRepo
+    public static String alleResultatC(String query) {
+        query = ("select bruker.fornavn, bruker.etternavn, c_resultat.år, c_resultat.periode, c_resultat.60w, c_resultat.bevegelighet, c_resultat.3000m, c_resultat.kroppshev, c_resultat.sargeant FROM ro.bruker, ro.c_resultat WHERE c_resultat.bruker_id=bruker.bruker_id AND bruker.klasse_id = 4 ORDER BY c_resultat.år;");
 
-      return query;
-    }
-
-    public static String søkFornavn(String query){
-        query = ("SELECT bruker.fornavn, bruker.etternavn, c_resultat.60w, c_resultat.bevegelighet, c_resultat.`3000m`, c_resultat.kroppshev, c_resultat.sargeant, c_resultat.år, c_resultat.periode FROM ro.c_resultat, ro.bruker WHERE bruker.bruker_id = c_resultat.bruker_id AND bruker.klasse_id = 4 AND bruker.fornavn = '?';");
         return query;
     }
 
+    // Query som henter ut resultat etter søking på fornavn
+    //Sorteres etter år
+    //Brukes i klassen SearchRepo
+    public static String søkFornavnC(String query) {
+        query = ("select bruker.fornavn, bruker.etternavn, c_resultat.år, c_resultat.periode, c_resultat.60w, c_resultat.bevegelighet, c_resultat.3000m, c_resultat.kroppshev, c_resultat.sargeant FROM ro.bruker, ro.c_resultat WHERE c_resultat.bruker_id=bruker.bruker_id AND bruker.fornavn = ? ORDER BY c_resultat.år;");
+
+        return query;
+
+    }
+
+    // Query som henter ut resultat etter søking på alle parametre
+    //Sorteres etter år
+    //Brukes i klassen SearchRepo
+    public static String søkAlleParamC(String query) {
+        query = ("select bruker.fornavn, bruker.etternavn, c_resultat.år, c_resultat.periode, c_resultat.60w, c_resultat.bevegelighet, c_resultat.3000m, c_resultat.kroppshev, c_resultat.sargeant FROM ro.bruker, ro.c_resultat WHERE c_resultat.bruker_id=bruker.bruker_id AND bruker.fornavn = ? AND bruker.etternavn = ? AND c_resultat.periode = ? AND bruker.kjønn = ? AND c_resultat.år = ? ORDER BY c_resultat.år;");
+
+        return query;
+    }
+
+    // Query som henter ut resultat etter søking på etternavn
+    //Sorteres etter år
+    //Brukes i klassen SearchRepo
+    public static String søkEtternavnC(String query) {
+        query = ("select bruker.fornavn, bruker.etternavn, c_resultat.år, c_resultat.periode, c_resultat.60w, c_resultat.bevegelighet, c_resultat.3000m, c_resultat.kroppshev, c_resultat.sargeant FROM ro.bruker, ro.c_resultat WHERE c_resultat.bruker_id=bruker.bruker_id AND bruker.etternavn = ? ORDER BY c_resultat.år;");
+        return query;
+    }
+
+    // Query som henter ut resultat etter søking på periode
+    //Sorteres etter år
+    //Brukes i klassen SearchRepo
+    public static String søkPeriodeC(String query) {
+        query = ("select bruker.fornavn, bruker.etternavn, c_resultat.år, c_resultat.periode, c_resultat.60w, c_resultat.bevegelighet, c_resultat.3000m, c_resultat.kroppshev, c_resultat.sargeant FROM ro.bruker, ro.c_resultat WHERE c_resultat.bruker_id=bruker.bruker_id AND c_resultat.periode = ? ORDER BY c_resultat.år;");
+        return query;
+    }
+
+    // Query som henter ut resultat etter søking på kjønn
+    //Sorteres etter år
+    //Brukes i klassen SearchRepo
+    public static String søkKjønnC(String query) {
+        query = ("select bruker.fornavn, bruker.etternavn, c_resultat.år, c_resultat.periode, c_resultat.60w, c_resultat.bevegelighet, c_resultat.3000m, c_resultat.kroppshev, c_resultat.sargeant FROM ro.bruker, ro.c_resultat WHERE c_resultat.bruker_id=bruker.bruker_id AND bruker.kjønn = ? ORDER BY c_resultat.år;");
+        return query;
+    }
+
+    public static String søkÅrC(String query) {
+        query = ("select bruker.fornavn, bruker.etternavn, c_resultat.år, c_resultat.periode, c_resultat.60w, c_resultat.bevegelighet, c_resultat.3000m, c_resultat.kroppshev, c_resultat.sargeant FROM ro.bruker, ro.c_resultat WHERE c_resultat.bruker_id=bruker.bruker_id AND c_resultat.år = ? ORDER BY c_resultat.år;");
+        return query;
+    }
+
+    public static String søkFornavnEtternavnC(String query) {
+        query = ("select bruker.fornavn, bruker.etternavn, c_resultat.år, c_resultat.periode, c_resultat.60w, c_resultat.bevegelighet, c_resultat.3000m, c_resultat.kroppshev, c_resultat.sargeant FROM ro.bruker, ro.c_resultat WHERE c_resultat.bruker_id=bruker.bruker_id AND bruker.fornavn = ? AND bruker.etternavn = ? ORDER BY c_resultat.år;");
+        return query;
+    }
+
+    public static String søkEtternavnKjønnC(String query) {
+        query = ("select bruker.fornavn, bruker.etternavn, c_resultat.år, c_resultat.periode, c_resultat.60w, c_resultat.bevegelighet, c_resultat.3000m, c_resultat.kroppshev, c_resultat.sargeant FROM ro.bruker, ro.c_resultat WHERE c_resultat.bruker_id=bruker.bruker_id AND bruker.kjønn = ? AND bruker.etternavn = ? ORDER BY c_resultat.år;");
+        return query;
+    }
 }
---
+++ src/main/java/servlets/søking/juniorC/HentPåFornavn_C.java
@@ -0,0 +1,51 @@
+package servlets.søking.juniorC;
+
+
+import servlets.tryms.AbstractAppServlet;
+import tools.repository.SearchRepo;
+import tools.repository.SearchRepoC;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HentPåFornavn_C", urlPatterns = "/HentPåFornavn_C")
+public class HentPåFornavn_C extends AbstractAppServlet {
+
+    @Override
+    protected void writeBody(HttpServletRequest req, PrintWriter out) {
+        out.println("<html><head><link rel='stylesheet' href='main.css'></head><body><h2> Resultat av søk i Junior C : </h2>");
+        out.println("<div class='tabellcss'>");
+        out.println("<hr></br><table cellspacing='0' cellpadding='5' border='1'>");
+        out.println("<tr>");
+        out.println("<td><b>Fornavn</b></td>");
+        out.println("<td><b>Etternavn</b></td>");
+        out.println("<td><b>År</b></td>");
+        out.println("<td><b>Testperiode</b></td>");
+        out.println("<td><b>60w</b></td>");
+        out.println("<td><b>Bevegelighet</b></td>");
+        out.println("<td><b>3000 meter</b></td>");
+        out.println("<td><b>Kroppshev</b></td>");
+        out.println("<td><b>Sargeant</b></td>");
+        out.println("</tr>");
+
+        out.println("<a href=resultatsøk-juniorC.jsp>Tilbake til resultat</a>");
+
+        out.println("<a href=resultatsøk.jsp>Tilbake til resultat</a>");
+        String fornavn = req.getParameter("fornavn");
+        SearchRepoC.søkFornavnC(fornavn, out);
+
+    }
+
+    @Override
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        writeResponse(request, response, "Hello!");
+    }
+
+
+
+}
